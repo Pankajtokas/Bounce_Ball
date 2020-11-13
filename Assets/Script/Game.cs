4e949299ba6012ecfcd9a4c6167db5b4d00d0280
Pankaj Tokas <Pankajtokas911@gmail.com>
2020-11-13 14:21:29 +0530

    First Commit

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    private const int V = 500;
+    private const string SceneName = "Level2";
+    Rigidbody rb;
+    public GameObject winText;
+    float xInput;
+    float zInput;
+
+     public float speed = 7000f;
+     public float left_right_movment = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Destroy(gameObject,5f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.position.x < Screen.width / 2)
+                {
+                    rb.AddForce(-left_right_movment * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                }
+                else if (touch.position.x > Screen.width / 2)
+                {
+                    rb.AddForce(left_right_movment * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                }
+            }
+
+        }
+       
+       /*Input = Input.GetAxis("Horizontal");
+       xInput = Input.GetAxis("Horizontal");
+
+       rb.AddForce(xInput * speed, 0, zInput * speed);
+       
+         for (int i =0; i<Input.touchCount; i++) {
+         Touch touch = Input.GetTouch(i);
+         
+         if (touch.position.x < Screen.width/2)
+         {
+             transform.Translate(Vector3.left * Time.deltaTime * PlayerMoveSpeed);
+         }
+         if (touch.position.x > Screen.width/2)
+         {
+             transform.Translate(Vector3.right * Time.deltaTime * PlayerMoveSpeed);*/
+
+    }
+    
+    public void OnMouseDown()
+	{
+       rb.AddForce(Vector3.up * 400);
+       // Destroy(gameObject);
+    }
+
+    public void QuitGame ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            winText.SetActive(true);
+                     
+            //Destroy(collision.gameObject);
+        }   
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+![image](https://user-images.githubusercontent.com/28135312/99045140-e61f0400-25b6-11eb-87ad-1a1441e4c9e8.png)
+
+
+To develop an android application app using Unity.
+
+    About Module
+    
+ Developing a Mini Game with Movement.
+ 
+ a. When user clicks the red ball, it should jump.
+
+ b. When user clicks on left side of screen, then red ball should move left.
+
+ c. When user clicks on the right side of the screen, then red ball should move right.
+    
+    Home Page
+![image](https://user-images.githubusercontent.com/28135312/99045706-c50ae300-25b7-11eb-9003-2ff2ed465c1b.png)
+
+    Instructions Page
+![image](https://user-images.githubusercontent.com/28135312/99045849-f683ae80-25b7-11eb-855a-af2ac292a0e4.png)
+ 
+    Game Page
+![image](https://user-images.githubusercontent.com/28135312/99046020-2d59c480-25b8-11eb-9345-1d69f7b53418.png)
+
+    Winning Page
+![image](https://user-images.githubusercontent.com/28135312/99046338-9e00e100-25b8-11eb-8e43-986e06016d3a.png)
